09c9350ee624479beb0d50907437786be1a31452
Ronalds Palacis <ronalds.palacis@outlook.com>
2022-06-11 14:56:42 +0300

    feat: added default animation states

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "@/components";
 import "@/styles/globals.css";
 import theme from "@/styles/theme";
 import { ThemeProvider } from "@mui/system";
+import { motion } from "framer-motion";
 import type { AppProps } from "next/app";
 import { ReactElement } from "react";
 import "../../public/fonts/style.css";
@@ -10,8 +11,10 @@ import "../../public/fonts/style.css";
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
-      <Component {...pageProps} />
+      <motion.div initial={"initial"} animate={"animate"}>
+        <Navbar />
+        <Component {...pageProps} />
+      </motion.div>
     </ThemeProvider>
   );
 }
